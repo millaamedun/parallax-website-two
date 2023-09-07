a0495863af52ac387b854a1ec886d2576757ad05
millaamedun <133498951+millaamedun@users.noreply.github.com>
2023-09-07 12:55:45 +0330

    Update Text.jsx

--- components/Text.jsx
+++ components/Text.jsx
@@ -6,9 +6,9 @@ function TextBlock() {
                 <h1 className="textblock-title">What is Firewatch?</h1>
                 <p className="textblock-content">
                 The year is 1989.<br/><br/>
-                You are a man named Henry who has retreated from your messy life to work as a fire lookout in the Wyoming wilderness. Perched atop a mountain, it's your job to find smoke and keep the wilderness safe.<br/><br/>
-                An especially hot, dry summer has everyone on edge. Your supervisor, a woman named Delilah, is available to youat all times over a small, handheld radio—and is your only contact with the world you've left behind.<br/><br/>
-                But when something strange draws you out of your lookout tower and into the world below, you'll explore a wild and unknown environment, facing questions and making interpersonal choices that can build or destroy the only meaningful relationship you have.
+                You are a man named Henry who has retreated from your messy life to work as a fire lookout in the Wyoming wilderness. Perched atop a mountain, its your job to find smoke and keep the wilderness safe.<br/><br/>
+                An especially hot, dry summer has everyone on edge. Your supervisor, a woman named Delilah, is available to youat all times over a small, handheld radio—and is your only contact with the world you have left behind.<br/><br/>
+                But when something strange draws you out of your lookout tower and into the world below, you will explore a wild and unknown environment, facing questions and making interpersonal choices that can build or destroy the only meaningful relationship you have.
                 </p>
             </div>
             <footer className='textblock-footer'> Created By<p className="textblock-mill" href="/">Millaamedun</p></footer>
